783944ac271bcbc75364defd1e1f2e98b01e5502
animesh <animeshchaudhri3@gmail.com>
2024-03-01 00:26:57 +0530

    fixed some more ig

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,10 @@
-import { useState } from 'react'
-import * as ReactDOM from "react-dom/client";
 import './App.css'
 import Home from "./Pages/Home.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <React.StrictMode>
-    <Home/>
-    </React.StrictMode>
-    
+      <Home/>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,32 +1,21 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import Chat from "./Pages/Chat.jsx"
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { io } from 'socket.io-client'
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path: "room/:id",
-    element: <Chat  />,  
-  },
-]);
 
 export const socket = io("https://chatsocketio-mcnp.onrender.com/");
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // // <React.StrictMode>
-  //   {/* <App /> */}
-  //   <>
-   
-  // {/* // </React.StrictMode>, */}
-  // </>
-  <RouterProvider router={router} />
-)
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App/>} />
+        <Route path="room/:id" element={<Chat/>} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
