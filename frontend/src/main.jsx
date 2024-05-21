df8d531deaacf179b2eed8b05071f348508fbb92
animesh <animeshchaudhri3@gmail.com>
2024-05-21 10:51:11 +0530

    level sabke niklege

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.jsx'
-import Chat from "./Pages/Chat/Chat.jsx"
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+import Chat from "./Pages/Chat/Chat.jsx";
+import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { io } from 'socket.io-client'
+import { io } from "socket.io-client";
 
-export const socket = io("https://chatsocketio-mcnp.onrender.com/");
+export const socket = io("https://desertfancier-watcher.toystack.dev/");
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App/>} />
-        <Route path="room/:id" element={<Chat/>} />
+        <Route path="/" element={<App />} />
+        <Route path="room/:id" element={<Chat />} />
       </Routes>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
